3dc8606500b35b7569b27902596ca8f3c3f31d88
ramofx <roman.dvorovoy123@gmail.com>
2023-11-12 13:13:20 +0100

    simplified: no redundant variables

--- src/Resources/Common/Utilities/Timing.php
+++ src/Resources/Common/Utilities/Timing.php
@@ -7,15 +7,19 @@ namespace App\Resources\Common\Utilities {
   class Timing {
     protected static array $timingStack = [];
 
+
+
     public static function start(): void {
       self::$timingStack[] = microtime(true);
     }
 
-    public static function stop() {
-      $durationMicroseconds = microtime(true) - array_pop(self::$timingStack);
-      $durationMilliseconds = round($durationMicroseconds * 1000, 1);
 
-      return $durationMilliseconds;
+
+    public static function stop(): float {
+      return round(
+        microtime(true) - array_pop(self::$timingStack) * 1000,
+        1
+      );
     }
   }
 }
