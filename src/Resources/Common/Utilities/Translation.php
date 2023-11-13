eff463cea43e5e88acda14bba4c8f1ef8ad016a1
ramofx <roman.dvorovoy123@gmail.com>
2023-11-13 22:37:11 +0100

    update: cleaned up, naming casing

--- src/Resources/Common/Utilities/Translation.php
+++ src/Resources/Common/Utilities/Translation.php
@@ -7,42 +7,46 @@ namespace App\Resources\Common\Utilities {
 
 
   class Translation {
+    protected const DEFAULT_LANGUAGE = 'en';
+    protected const SUPPORTED_LANGUAGES = [ 'en', 'cs', 'de' ];
+
+
+
     public static function getPreferredLanguage(): string {
-      $supported_languages = ["cs", "en", "de"];
-      $preferred_language = $_SERVER["HTTP_PREFERRED_LANGUAGE"] ?? null;
-      $accept_language = $_SERVER["HTTP_ACCEPT_LANGUAGE"] ?? null;
+      $preferredLanguage = $_SERVER['HTTP_PREFERRED_LANGUAGE'] ?? null;
+      $acceptLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? null;
 
-      if ($preferred_language !== null && in_array($preferred_language, $supported_languages)) {
-        Debug::set('final-language-from', 'preferred-language');
+      if ($preferredLanguage !== null && in_array($preferredLanguage, self::SUPPORTED_LANGUAGES))
+        return $preferredLanguage;
 
-        return $preferred_language;
-      } else if ($accept_language !== null) {
-        $accept_languages = explode(',', $accept_language);
+      if ($acceptLanguage === null)
+        return self::DEFAULT_LANGUAGE;
 
-        foreach ($accept_languages as $l) {
-          $sl = substr($l, 0, 2);
+      $acceptLanguages = explode(',', $acceptLanguage);
 
-          if (in_array($sl, $supported_languages)) {
-            Debug::set('final-language-from', 'accept-language');
+      foreach ($acceptLanguages as $language) {
+        $languageTag = substr($language, 0, 2);
 
-            return $sl;
-          }
-        }
+        if (in_array($languageTag, self::SUPPORTED_LANGUAGES))
+          return $languageTag;
       }
 
-      Debug::set('final-language-from', 'fallback');
-
-      return "cs";
+      return self::DEFAULT_LANGUAGE;
     }
 
-    public static function translate(array $translated_messages): string {
+
+
+    public static function translate(array $translatedMessages): string {
       $language = self::getPreferredLanguage();
 
-      if (array_key_exists($language, $translated_messages)) {
-        return $translated_messages[$language];
-      } else {
-        return "$translated_messages[0] (no $language translation exist for this message, message provided has " . join(', ', array_keys($translated_messages)) . " languages)";
-      }
+      if (array_key_exists($language, $translatedMessages))
+        return $translatedMessages[$language];
+
+      $availableTranslations = join(', ', array_keys($translatedMessages));
+      $defaultTranslation = $translatedMessages[self::DEFAULT_LANGUAGE] ?? reset($translatedMessages);
+      $note = "Please note, that current message wasn't translated (requested: $language, supported: $availableTranslations";
+
+      return "$defaultTranslation $note";
     }
   }
 }
